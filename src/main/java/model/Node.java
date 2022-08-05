edf45bf277b9944bbecc10c0bc407c398511094b
Franklyn <franklyn.narvaez@correounivalle.edu>
2022-08-04 20:48:38 -0500

    Cambios en la clase Node

--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -5,9 +5,11 @@ public class Node {
     private Node parent;
     private Integer deep;
     private Integer[][] positions = new Integer[2][2];
-    private Integer[] points = new Integer[2];
+    private Integer[] points = new Integer[2]; //[0] para maquina,[1] para jugador
     private Integer utility = 0;
     private Integer kind; //0 max, 1 min
+
+    private Knight[] knights = new Knight[2]; //[0] para maquina,[1] para jugador
 
 
     /**
@@ -21,9 +23,11 @@ public class Node {
         this.deep = 0;
         this.points[0] = 0;
         this.points[1] = 0;
-        this.positions = positions;
+        this.knights[0] = new Knight(positions[0][0], positions[0][1]);
+        this.knights[1] = new Knight(positions[1][0], positions[0][1]);
         this.kind = 0;
         this.utility = Integer.MIN_VALUE;
+
     }
 
 
@@ -33,14 +37,21 @@ public class Node {
      * @param parent    parent node
      * @param direction 1 right, 2 left, 3 up and other down
      */
-    public Node(Node parent, Integer direction) {
+    public Node(Node parent, Integer direction, Integer[] position) {
         this.parent = parent;
         this.kind = parent.getKind() == 0? 1 : 0;
         this.utility = kind == 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
         this.deep = parent.getDeep() + 1;
-        //this.map = parent.nextMap();
+        this.knights[0] = kind == 0 ? new Knight(parent.getKnights()[0].getPlace()[0], parent.getKnights()[0].getPlace()[1]) : new Knight(position[0], position[1]);
+        this.knights[1] = kind == 0 ? new Knight(position[0], position[1]) : new Knight(parent.getKnights()[1].getPlace()[0], parent.getKnights()[1].getPlace()[1]);
+        this.map = parent.nextMap(position);
+        this.points[0] = kind == 0 ? parent.getPoints()[0] : parent.getPoints()[0] + parent.nextPoint(position);
+        this.points[1] = kind == 0 ? parent.getPoints()[1] + parent.nextPoint(position) : parent.getPoints()[1];
     }
 
+    public Knight[] getKnights() {
+        return knights;
+    }
 
     public Node getParent() {
         return parent;
@@ -77,20 +88,29 @@ public class Node {
      * @param newMap environment
      * @return robot place
      */
-    public static Integer[] initialPlace(Integer[][] newMap) {
+    public static Integer[][] initialPlace(Integer[][] newMap) {
         if (newMap == null) {
             return null;
         }
-        Integer[] place = new Integer[2];
+        Integer[][] place = new Integer[2][2];
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (newMap[i][j] == 2) {
-                    place[0] = i;
-                    place[1] = j;
+                    place[0][0] = i;
+                    place[0][1] = j;
+                } else if (newMap[i][j] == 1) {
+                    place[1][0] = i;
+                    place[1][1] = j;
                 }
             }
         }
         return place;
+    }
+    public Integer nextPoint(Integer[] position){
+        if(map[position[0]][position[1]] > 2){
+            return map[position[0]][position[1]];
+        }
+        return 0;
     }
 
     /**
@@ -101,23 +121,22 @@ public class Node {
         return true;
     }
 
-
     /**
-     * @param direction 1 right, 2 left, 3 up and other down
-     * @return position on next move
-     */
-    public Integer[] nextPlace(Integer direction) {
-        Integer[] nextPlace = new Integer[2];
-        return nextPlace;
-    }
-
-
-    /**
-     * @param nextPlace position on next move
+     * @param position position on next move
      * @return map after movement
      */
-    public Integer[][] nextMap(Integer[] nextPlace) {
+    public Integer[][] nextMap(Integer[] position) {
         Integer[][] nextMap = new Integer[8][8];
+        for(int i = 0; i < 8; i++){
+            System.arraycopy(map[i], 0, nextMap[i], 0, 8);
+        }
+        if(kind == 0){
+            nextMap[position[0]][position[1]] = 2;
+            nextMap[positions[0][0]][positions[0][1]] = 0;
+        }else{
+            nextMap[position[0]][position[1]] = 1;
+            nextMap[positions[1][0]][positions[1][1]] = 0;
+        }
         return nextMap;
     }
 
